d62d77db39ed55dc21ce7d91a134ec753ab4446a
arc-015 <chavanavinash015@gmail.com>
2022-11-16 15:41:12 +0530

    Added :print cheap hotels in in weekend Hotel ReserVation System

--- src/com/bridgelabz/hotelreservation/HotelReservationMain.java
+++ src/com/bridgelabz/hotelreservation/HotelReservationMain.java
@@ -24,5 +24,6 @@ public class HotelReservationMain {
 
         HotelReservationSystem check = new HotelReservationSystem();
         check.cheapestHotelinWeekdays();
+        check.cheapestHotelinWeekend();
     }
 }
