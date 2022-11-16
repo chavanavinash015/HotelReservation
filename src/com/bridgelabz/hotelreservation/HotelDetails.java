d9495b02a07cd1b34105410468d9cf1152661afd
arc-015 <chavanavinash015@gmail.com>
2022-11-16 08:10:46 +0530

    Added :Welcome to the Hotel ReserVation System

---
+++ src/com/bridgelabz/hotelreservation/HotelDetails.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.hotelreservation;
+
+public class HotelDetails {
+
+
+
+}
---
+++ src/com/bridgelabz/hotelreservation/HotelReservationMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.hotelreservation;
+
+public class HotelReservationMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Reservation Program ");
+
+
+    }
+}
